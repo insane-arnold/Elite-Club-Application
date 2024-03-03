946b113301c5a55e43c99d54ab3d9640d269b8ad
insane_arnold <limbunihal07@gmail.com>
2024-03-03 19:49:49 +0530

    added some dependencies in pom.xml

--- eliteclub/src/main/java/com/club/eliteclub/EliteclubApplication.java
+++ eliteclub/src/main/java/com/club/eliteclub/EliteclubApplication.java
@@ -1,12 +1,16 @@
 package com.club.eliteclub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EliteclubApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(EliteclubApplication.class);
 	public static void main(String[] args) {
+		logger.info("Starting EliteClubApplication ....");
 		SpringApplication.run(EliteclubApplication.class, args);
 	}
 
