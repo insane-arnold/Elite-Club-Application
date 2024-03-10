32c30b4c6586d9b06f5101b6c88c12d32bfd63f1
insane_arnold <limbunihal07@gmail.com>
2024-03-10 20:20:56 +0530

    PostRequestMapping

--- eliteclub/src/main/java/com/club/eliteclub/initializer/Forbes400Properties.java
+++ eliteclub/src/main/java/com/club/eliteclub/initializer/Forbes400Properties.java
@@ -4,7 +4,6 @@ import org.hibernate.validator.constraints.URL;
 
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
